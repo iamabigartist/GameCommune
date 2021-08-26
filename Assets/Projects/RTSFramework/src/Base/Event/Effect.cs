f6f8502382b90dff86b68fe69676cef3e37d414d
yangchenghua <53459343+iamabigartist@users.noreply.github.com>
2021-08-26 18:24:55 +0800

    更新wiki关于整个游戏处理流程的解释

--- Assets/Projects/RTSFramework/src/Base/Event/Effect.cs
+++ Assets/Projects/RTSFramework/src/Base/Event/Effect.cs
@@ -6,15 +6,15 @@ namespace RTSFramework
         public enum EffectType
         {
             /// <summary>
-            ///     The event will remain and be processed.
+            ///     The effect will remain and be processed.
             /// </summary>
             Enabled,
             /// <summary>
-            ///     The event will remain but not be processed.
+            ///     The effect will remain but not be processed.
             /// </summary>
             Disabled,
             /// <summary>
-            ///     The event will be processed and removed from list afterwards.
+            ///     The effect will be processed and removed from list afterwards.
             /// </summary>
             Temporary
         }
