e26769e71513891ebc4a7bbfa802c6dd3e9a1c89
yangchenghua <53459343+iamabigartist@users.noreply.github.com>
2021-09-07 09:52:18 +0800

    add thought about field

--- Assets/Projects/RTSFramework v1_0/DataBase/PrimitiveField.cs
+++ Assets/Projects/RTSFramework v1_0/DataBase/PrimitiveField.cs
@@ -5,4 +5,7 @@ namespace RTSFramework_v1_0.DataBase
         public PrimitiveField(T value) { this.value = value; }
         public T value;
     }
+
+    ///Find a way to store the relation between field and gameobject/database
+    /// and a way to express the meaning of the field itself
 }
